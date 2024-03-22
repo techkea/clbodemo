bb704c43d938a2c21a2dc6e8692d82a484bfdebf
clbokea <clbo@kea.dk>
2024-03-22 10:45:56 +0100

    init

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String index(){
         return "Hello world";
     }
+
+    @GetMapping("/about")
+    public String about(){
+        return "About";
+    }
 }
